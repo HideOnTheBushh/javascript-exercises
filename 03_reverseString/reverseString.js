25b16af6896c86085e686a87b63da11dfd184150
HideOnTheBushh <tonyzika@seznam.cz>
2024-12-03 21:08:08 +0100

    Complete exercises 1-4
    Forgot to commit, completed 4 exercises.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const letters = [];
+    let new_string = "";
+    length = string.length;
 
+    for (let i = 0; i < length; i++) {
+        letters[i] = string[length - i - 1];
+    }
+    for (let i = 0; i < length; i++) {
+        new_string += letters[i];
+    }
+    return new_string;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
 const removeFromArray = function() {
+    const array = arguments[0];
+    const new_array = [];
+  
+    outer: for (let i = 0; i < array.length; i++) {
+        for (let j = 1; j < arguments.length; j++) {
+            if (array[i] === arguments[j]) {
+                continue outer;
+            }   
+        }
+        new_array.push(array[i]);
+    }
+
+    return new_array;
 };
 
 // Do not edit below this line
