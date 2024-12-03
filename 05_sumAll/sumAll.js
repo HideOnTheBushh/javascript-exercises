5029b7e39f8e2d9ff8623e542db727dc937f454f
HideOnTheBushh <tonyzika@seznam.cz>
2024-12-03 21:20:51 +0100

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    } else if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    // First number is smaller
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    // First number is larger
+    for (let i = end; i <= start; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
