535d840d82f2951f155b402ac45f6aa23b1a1d22
HideOnTheBushh <tonyzika@seznam.cz>
2024-12-05 20:29:32 +0100

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+  deg -= 32;
+  deg *= 5 / 9;
+  return Math.round(deg * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+  deg *= 9 / 5;
+  deg += 32;
+  return Math.round(deg * 10) / 10;
 };
 
 // Do not edit below this line
