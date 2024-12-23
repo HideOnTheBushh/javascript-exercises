fa37e9d99126b3b8a04054f0523c7fcfb7bd3ac8
HideOnTheBushh <tonyzika@seznam.cz>
2024-12-23 17:04:26 +0100

    Complete 08 - 12 exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // Convert into lower case
+    let old = str.toLowerCase();
+    // Convert into array of letters only
+    old = old
+            .split("")
+            .filter(letter => isAlphanumeric(letter))
+            .join("");
+    // Check whether same as reversed version
+    return old === old.split("").reverse().join("");
 };
+
+function isAlphanumeric(symbol) {
+    // Check if the symbol is a single character and is alphanumeric (letters or digits)
+    return /^[a-zA-Z0-9]$/.test(symbol);
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let newArr = arr.sort((a, b) => {
+        let aAge;
+        let bAge;
+        if (a.yearOfDeath) {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        } else {
+            aAge = new Date().getFullYear() - a.yearOfBirth;
+        }
+        if (b.yearOfDeath) {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        } else {
+            bAge = new Date().getFullYear() - b.yearOfBirth;
+        }
+        return bAge - aAge;
 
+    })
+    return newArr[0];
 };
 
 // Do not edit below this line
